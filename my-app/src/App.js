6852c5ea72878cdb0171d4cedb2efc225c632829
email2ify <email2ify@yahoo.com>
2023-05-04 19:48:54 +0000

    create Stateful functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import StatefulGreeting from './components/StatefulGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-     
-        <FunctionalGreetingWithProps name="Mike" message="I see you're" age="32 years old," greeting="Nice to meet you!!"/>
+      <StatefulGreeting greeting=""/>
+      <FunctionalGreetingWithProps name="Mike" message="I see you're" age="32 years old," greeting="Nice to meet you!!"/>
 
     </div>
   );
